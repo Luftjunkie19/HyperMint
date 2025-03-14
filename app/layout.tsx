209439663482cdf864b9272c145a5a4607b7fcee
Luftjunkie19 <kshjssjjs@gmail.com>
2025-03-15 00:02:38 +0100

    Add Latin subset to Poppins font configuration in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import WagmiConfigProvider from "@/providers/WagmiConfigProvider";
 
 export const poppins = Poppins({
   variable: "--font-poppins",
-
+subsets: ["latin"],
   weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
